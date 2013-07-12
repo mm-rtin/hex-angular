e0474f19c78f0d33d0c191a3830523bcf293e7fd
codecollision <michael@codecollision.com>
2013-07-12 00:43:09 -0700

    drag scrolling/paging prototype

--- scripts/directives/content_tabs.js
+++ scripts/directives/content_tabs.js
@@ -27,6 +27,9 @@ App.directive('contentTabs', ['$rootScope', '$timeout', '$q', function($rootScop
                 tabContainerAtStart = false,
                 tabContainerAtEnd = false;
 
+            // objects
+            var scroller = null;
+
             // jquery elements
             var $htmlRoot = $('html'),
                 $contentTabs = $element,
@@ -61,32 +64,6 @@ App.directive('contentTabs', ['$rootScope', '$timeout', '$q', function($rootScop
                 renderContentTabs();
             }
 
-            (function() {
-                var lastTime = 0;
-                var vendors = ['ms', 'moz', 'webkit', 'o'];
-                for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-                    window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-                    window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
-                                               || window[vendors[x]+'CancelRequestAnimationFrame'];
-                }
-
-                if (!window.requestAnimationFrame)
-                    window.requestAnimationFrame = function(callback, element) {
-                        var currTime = new Date().getTime();
-                        var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-                        var id = window.setTimeout(function() { callback(currTime + timeToCall); },
-                          timeToCall);
-                        lastTime = currTime + timeToCall;
-                        return id;
-                    };
-
-                if (!window.cancelAnimationFrame)
-                    window.cancelAnimationFrame = function(id) {
-                        clearTimeout(id);
-                    };
-            }());
-
-
              /* createEventHandlers -
             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
             function createEventHandlers() {
@@ -96,57 +73,133 @@ App.directive('contentTabs', ['$rootScope', '$timeout', '$q', function($rootScop
 
                     // update window height
                     windowWidth = $(window).width();
+
+                    updateBoundingBox(false);
                 });
 
-                // content gallery: drag
-                $tabsContainer.hammer().on('drag', function(e) {
 
-                    var delta = e.gesture.deltaX;
+                if ('ontouchstart' in window) {
 
-                    rafId = requestAnimationFrame(function() {
-                        console.log(delta);
-                        dragTabContainer(delta);
-                    });
+                    $contentTabs[0].addEventListener("touchstart", function(e) {
 
-                    e.gesture.preventDefault();
-                    e.preventDefault();
-                });
+                        // Don't react if initial down happens on a form element
+                        if (e.target.tagName.match(/input|textarea|select/i)) {
+                            return;
+                        }
 
-                // content gallery: drag end
-                $tabsContainer.hammer().on('dragend', function(e) {
-                    lastDelta = 0;
-                    e.gesture.preventDefault();
-                    e.preventDefault();
-                });
+                        scroller.doTouchStart(e.touches, e.timeStamp);
+                        e.preventDefault();
+                    }, false);
 
-                // content gallery: tap
-                // $tabsContainer.hammer().on('tap', function(e) {
+                    document.addEventListener("touchmove", function(e) {
+                        scroller.doTouchMove(e.touches, e.timeStamp);
+                    }, false);
 
-                // });
+                    document.addEventListener("touchend", function(e) {
+                        scroller.doTouchEnd(e.timeStamp);
+                    }, false);
 
-                // // content gallery: swipeleft
-                // $contentTabs.hammer({'swipe_velocity': SWIPE_VELOCITY}).on('swipeleft', function(e) {
+                } else {
 
-                //     $rootScope.safeApply(function() {
-                //         setActiveTab($scope.state.currentTabIndex + 1);
-                //     });
-                // });
+                    var mousedown = false;
 
-                // // content gallery: swiperight
-                // $contentTabs.hammer({'swipe_velocity': SWIPE_VELOCITY}).on('swiperight', function(e) {
-                // });
+                    $contentTabs[0].addEventListener("mousedown", function(e) {
+                        // Don't react if initial down happens on a form element
+                        if (e.target.tagName.match(/input|textarea|select/i)) {
+                            return;
+                        }
 
-                // // sliderContainer: transitionend
-                // $contentTabs.bind('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd msTransitionEnd', function() {
+                        scroller.doTouchStart([{
+                            pageX: e.pageX,
+                            pageY: e.pageY
+                        }], e.timeStamp);
 
-                // });
+                        mousedown = true;
+                    }, false);
+
+                    document.addEventListener("mousemove", function(e) {
+                        if (!mousedown) {
+                            return;
+                        }
+
+                        scroller.doTouchMove([{
+                            pageX: e.pageX,
+                            pageY: e.pageY
+                        }], e.timeStamp);
+
+                        mousedown = true;
+                    }, false);
+
+                    document.addEventListener("mouseup", function(e) {
+                        if (!mousedown) {
+                            return;
+                        }
+
+                        scroller.doTouchEnd(e.timeStamp);
+
+                        mousedown = false;
+                    }, false);
+
+                }
             }
+
+
+            /* DOM-based rendering (Uses 3D when available, falls back on margin when transform not available) */
+            var render = (function(global) {
+
+                var docStyle = document.documentElement.style;
+
+                var engine;
+                if (global.opera && Object.prototype.toString.call(opera) === '[object Opera]') {
+                    engine = 'presto';
+                } else if ('MozAppearance' in docStyle) {
+                    engine = 'gecko';
+                } else if ('WebkitAppearance' in docStyle) {
+                    engine = 'webkit';
+                } else if (typeof navigator.cpuClass === 'string') {
+                    engine = 'trident';
+                }
+
+                var vendorPrefix = {
+                    trident: 'ms',
+                    gecko: 'Moz',
+                    webkit: 'Webkit',
+                    presto: 'O'
+                }[engine];
+
+                var helperElem = document.createElement("div");
+                var undef;
+
+                var perspectiveProperty = vendorPrefix + "Perspective";
+                var transformProperty = vendorPrefix + "Transform";
+
+                if (helperElem.style[perspectiveProperty] !== undef) {
+
+                    return function(left, top, zoom) {
+                        $tabsContentContainer[0].style[transformProperty] = 'translate3d(' + (-left) + 'px,' + (-top) + 'px,0) scale(' + zoom + ')';
+                    };
+
+                } else if (helperElem.style[transformProperty] !== undef) {
+
+                    return function(left, top, zoom) {
+                        $tabsContentContainer[0].style[transformProperty] = 'translate(' + (-left) + 'px,' + (-top) + 'px) scale(' + zoom + ')';
+                    };
+
+                } else {
+
+                    return function(left, top, zoom) {
+                        $tabsContentContainer[0].style.marginLeft = left ? (-left/zoom) + 'px' : '';
+                        $tabsContentContainer[0].style.marginTop = top ? (-top/zoom) + 'px' : '';
+                        $tabsContentContainer[0].style.zoom = zoom || '';
+                    };
+
+                }
+            })(window);
+
 
             /* renderContentTabs -
             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
             function renderContentTabs() {
-
-                createEventHandlers();
 
                 // calculate tab content widths
                 $scope.state.tabCount = $tabsContent.length;
@@ -168,6 +221,21 @@ App.directive('contentTabs', ['$rootScope', '$timeout', '$q', function($rootScop
                 $tabsContent.css({
                     'width': $scope.state.tabContentWidth + '%'
                 });
+
+
+                setTimeout(function() {
+
+                    // Initialize Scroller
+                    scroller = new Scroller(render, {
+                        scrollingY: false,
+                        paging: true,
+                        bouncing: false
+                    });
+
+                    updateBoundingBox(true);
+
+                    createEventHandlers();
+                }, 1000);
             }
 
             /* setTabsContentContainerStyle -
@@ -175,59 +243,33 @@ App.directive('contentTabs', ['$rootScope', '$timeout', '$q', function($rootScop
             function setTabsContentContainerStyle(width, translateAmount) {
 
                 $tabsContentContainer.css({
-                    'width': width + '%',
-                    '-webkit-transform': 'translate3d(' + -translateAmount + '%, 0px, 0px)',
-                    '-moz-transform': 'translate3d(' + -translateAmount + '%, 0px, 0px)',
-                    '-ms-transform': 'translate(' + -translateAmount + '%, 0px)',
-                    '-o-transform': 'translate3d(' + -translateAmount + '%, 0px, 0px)',
-                    'transform': 'translate3d(' + -translateAmount + '%, 0px, 0px)'
+                    'width': width + '%'
                 });
             }
 
+            function updateBoundingBox(updatePosition) {
+
+                var container = $contentTabs[0];
+                var content = $tabsContentContainer[0];
+
+                // Setup Scroller
+                var rect = container.getBoundingClientRect();
+
+                if (updatePosition) {
+                    scroller.setPosition(rect.left+container.clientLeft, rect.top+container.clientTop);
+                }
+
+                scroller.setDimensions(container.clientWidth, container.clientHeight, content.offsetWidth, content.offsetHeight);
+                scroller.setSnapSize($($tabsContent[0]).width(), 100);
+            }
 
             /* setTabsContainerStyle -
             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
             function setTabsContainerStyle(width, translateAmount) {
 
                 $tabsContainer.css({
-                    'width': width + 'px',
-                    '-webkit-transform': 'translate3d(' + translateAmount + 'px, 0px, 0px)',
-                    '-moz-transform': 'translate3d(' + translateAmount + 'px, 0px, 0px)',
-                    '-ms-transform': 'translate(' + translateAmount + 'px, 0px)',
-                    '-o-transform': 'translate3d(' + translateAmount + 'px, 0px, 0px)',
-                    'transform': 'translate3d(' + translateAmount + 'px, 0px, 0px)'
+                    'width': width + 'px'
                 });
-            }
-
-            /* dragTabContainer -
-            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-            function dragTabContainer(delta) {
-
-                var tabsContainerWidth = $contentTabs.width();
-                var negativeLimit = 500;
-
-                tabContainerPosition += delta - lastDelta;
-
-                tabContainerAtStart = false;
-                tabContainerAtEnd = false;
-
-                // restrict scroll down amount
-                // if (tabContainerPosition <= negativeLimit) {
-                //     tabContainerPosition = negativeLimit;
-                //     tabContainerAtStart = true;
-                //     tabContainerAtEnd = false;
-                // }
-
-                // // restrict scroll up amount
-                // if (tabContainerPosition >= 0) {
-                //     tabContainerPosition = 0;
-                //     tabContainerAtStart = false;
-                //     tabContainerAtEnd = true;
-                // }
-
-                lastDelta = delta;
-
-                setTabsContainerStyle($scope.state.tabsContainerWidth, tabContainerPosition);
             }
 
             /* setActiveTab -
